31ee959a5e7b2b1820749a53b0766465e6905d24
mattdesl <dave.des@gmail.com>
2015-06-12 10:37:48 -0400

    add example without a close() func

---
+++ test/exampe.js
@@ -0,0 +1,9 @@
+var test = require('tape')
+
+test('should do something', function (t) {
+  t.plan(1)
+
+  setTimeout(function () {
+    t.deepEqual([ 0, 0 ], [ 0, 0 ], 'are the same')
+  }, 50)
+})
