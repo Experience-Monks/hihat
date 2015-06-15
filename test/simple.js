e57339381d230a5100c39be35371ed07693ba215
mattdesl <dave.des@gmail.com>
2015-06-15 11:42:32 -0400

    ensure __dirname is accurate; downgrade tap-spec since its 4.0 seems unstable

--- test/simple.js
+++ test/simple.js
@@ -1,11 +1,14 @@
 /*global HTMLElement*/
 var test = require('tape')
+var path = window.require('path')
 
 test('test HTML element', function (t) {
   t.equal(3, 3)
   t.equal(3, 3)
   t.ok(document.body instanceof HTMLElement,
       'should be an element')
+
+  t.equal(__dirname, path.join(window.process.cwd(), 'test'), '--no-commondir for bundles')
   t.end()
 
   setTimeout(function () {
