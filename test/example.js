06224d95c0b65913c8658b775a2a3fb968d3d71d
mattdesl <dave.des@gmail.com>
2015-06-16 11:34:22 -0400

    cleaner syntax error handling

--- test/example.js
+++ test/example.js
@@ -4,6 +4,6 @@ test('should do something', function (t) {
   t.plan(1)
 
   setTimeout(function () {
-    t.deepEqual([ 0, 0 ], [ 0, 0 ], 'are the same')
+    t.deepEqual([ 0,; 0 ], [ 0, 0 ], 'are the same')
   }, 50)
 })
