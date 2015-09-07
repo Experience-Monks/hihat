ae500ef185b5a5e30be11bfb4c186731731c1c02
Vincent Weevers <mail@vincentweevers.nl>
2015-09-07 10:14:51 +0200

    Filename expansion for less fortunate shells

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,10 +3,17 @@ var args = process.argv.slice(2)
 var path = require('path')
 var parseArgs = require('../lib/parse-args')
 var spawn = require('../spawn')
+var globby = require('globby')
 
 var argv = parseArgs(args)
 var server = path.resolve(__dirname, 'server.js')
+var patterns = argv.entries
 
-spawn(server, args, {
-  rawOutput: argv.rawOutput
+globby(patterns).then(function (entries) {
+  var rest = args.slice(patterns.length)
+  args = entries.concat(rest)
+
+  spawn(server, args, {
+    rawOutput: argv.rawOutput
+  })
 })
