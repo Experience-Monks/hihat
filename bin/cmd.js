9c49b3507463588cf2dd0f9c2104377067920782
mattdesl <dave.des@gmail.com>
2015-09-08 13:50:31 -0400

    allow glob pattern in entries

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,17 +3,10 @@ var args = process.argv.slice(2)
 var path = require('path')
 var parseArgs = require('../lib/parse-args')
 var spawn = require('../spawn')
-var globby = require('globby')
 
 var argv = parseArgs(args)
 var server = path.resolve(__dirname, 'server.js')
-var patterns = argv.entries
 
-globby(patterns).then(function (entries) {
-  var rest = args.slice(patterns.length)
-  args = entries.concat(rest)
-
-  spawn(server, args, {
-    rawOutput: argv.rawOutput
-  })
+spawn(server, args, {
+  rawOutput: argv.rawOutput
 })
